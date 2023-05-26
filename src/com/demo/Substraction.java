5d08311d68bf68a15058c85543f05b509438c6fa
sonamkolhekar <sonamkolhekar@gmail.com>
2023-05-26 15:28:43 +0530

    second commit

--- src/com/demo/Substraction.java
+++ src/com/demo/Substraction.java
@@ -3,8 +3,12 @@ package com.demo;
 public class Substraction {
 
 	public static void main(String[] args) {
+		// data add from eclips by user
 		int c = 20;
 		int d = 10;
+		System.out.println("Substraction " + (c - d));
+		c = 30;
+		d = 20;
 		System.out.println("Substraction " + (c - d));
 
 	}
