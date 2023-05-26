0027c20c11317f29d0911d28388d3c5b4ae4dcab
sonamkolhekar <sonamkolhekar@gmail.com>
2023-05-26 15:25:44 +0530

    first commit

---
+++ src/com/demo/Substraction.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		int c = 20;
+		int d = 10;
+		System.out.println("Substraction " + (c - d));
+
+	}
+
+}
